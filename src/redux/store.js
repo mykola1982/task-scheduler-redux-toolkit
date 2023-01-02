a3ec7c218f44fe9652b55c530db28a8366bccd68
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-01-02 22:51:56 +0200

    add toolkit2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,14 @@
 //=============== Before ========================
 
-// import { createStore } from 'redux';
-// import { devToolsEnhancer } from '@redux-devtools/extension';
-// import { rootReducer } from './reducer';
-
-// const enhancer = devToolsEnhancer();
-// export const store = createStore(rootReducer, enhancer);
-
-//=============== After ========================
-import { configureStore } from '@reduxjs/toolkit';
+import { createStore } from 'redux';
+import { devToolsEnhancer } from '@redux-devtools/extension';
 import { rootReducer } from './reducer';
 
-const store = configureStore({ reducer: rootReducer });
+const enhancer = devToolsEnhancer();
+export const store = createStore(rootReducer, enhancer);
+
+//=============== After ========================
+// import { configureStore } from '@reduxjs/toolkit';
+// import { rootReducer } from './reducer';
+
+// const store = configureStore({ reducer: rootReducer });
